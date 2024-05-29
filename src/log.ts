ce5aaf52e48c3ad776c12e67a8c3bcd0bf707efd
Christian Nuss <christian.nuss@gmail.com>
2024-05-29 10:58:09 -0400

    clean up logging

--- src/log.ts
+++ src/log.ts
@@ -1,9 +1,9 @@
 import packageJson from "../package.json";
 
-export const log = (message?: any, ...optionalParams: any[]): void => {
+export const log = (message: any, obj?: Record<string, any>): void => {
   if (!process.env.SLY_DEBUG) return;
   console.log(
     `[${packageJson.name}@${packageJson.version}] ${message}`,
-    optionalParams
+    JSON.stringify(obj)
   );
 };
